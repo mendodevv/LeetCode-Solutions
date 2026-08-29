6f9c601986207ebea4b13fc22cae9eac2f3c41e3
mendodev <129572136+mendodevv@users.noreply.github.com>
2026-08-29 13:05:55 -0600

    Create twoSum.java
    Adds a twoSum method to find indices of two numbers that add up to a target value.

---
+++ algorithms/java/twoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int diff = 0;
+        int[] ans = {0,0};
+
+        for(int i = 0; i < nums.length; i++){
+            diff = target - nums[i];
+
+            if(map.containsKey(diff)){
+                return new int[] {i, map.get(diff)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
